a163a20af496ded7fbbab7cbf49661cb89ac5342
Siam Ahmed <sieam.khan12@gmail.com>
2026-03-05 15:12:40 +0600

    banner image add

--- src/Componenets/Banner/Banner.jsx
+++ src/Componenets/Banner/Banner.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import "./banner.css";
+import vector1 from "../../assets/vector1.png";
 export default function Banner({progress, resolved}) {
 
   return (
@@ -9,7 +10,14 @@ export default function Banner({progress, resolved}) {
         <span className='text-sm md:text-6xl font-semibold'>{progress.length}</span>
     </div>
     
- <div className=" banner-section-second rounded-sm  flex-1 mb-10 flex flex-col items-center justify-center">
+ <div className="rounded-sm  flex-1 mb-10 flex flex-col items-center justify-center" style={{
+    backgroundImage: `url(${vector1})`,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    backgroundColor: "#54CF68",
+    height: "250px",
+    color: "#FFFFFF"
+  }}>
         <p className='text-sm md:text-2xl'>Resolved</p>
         <span className=' text-sm md:text-6xl font-semibold'>{resolved.length}</span>
     </div>
